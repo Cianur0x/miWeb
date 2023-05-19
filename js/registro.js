1062df77446cd835bfaa35b5e65672ff56ec1166
CianLinux <pikabz2638@gmail.com>
2023-05-19 13:03:01 +0200

    alertas y reset form

--- js/registro.js
+++ js/registro.js
@@ -1,47 +1,58 @@
 function validarFormulario() {
-    var user = document.getElementById("user").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("pass").value;
-    var password2 = document.getElementById("pass2").value;
-    var terminos = document.getElementById("acepto").checked;
+  var user = document.getElementById("user").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pass").value;
+  var password2 = document.getElementById("pass2").value;
+  var terminos = document.getElementById("acepto").checked;
 
-    if (user === "" || email === "" || password === "" || password2 === "") {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error...',
-        text: 'Rellena los campos vacíos'
-      })
-      // return false;
-    }
+  if (user === "" || email === "" || password === "" || password2 === "") {
 
-    if (password2 != password) {
-      
-      Swal.fire({
-        icon: 'error',
-        title: 'Error...',
-        text: 'Las contraseñas no conciden!'
-      })
+    Swal.fire({
+      icon: "error",
+      title: "Error...",
+      text: "Rellene los campos vacíos.",
+    });
 
-    }
-  
-    if(terminos != true){
+  } else if (password2 != password) {
 
-      Swal.fire({
-        title: 'Custom width, padding, color, background.',
-        width: 600,
-        padding: '3em',
-        color: '#fff',
-        background: '#004671 url(/images/trees.png)',
-        backdrop: `
-          
+    Swal.fire({
+      icon: "error",
+      title: "Error...",
+      text: "Las contraseñas no conciden!",
+    });
+
+  } else if (terminos != true) {
+
+    Swal.fire({
+      title: "Acepte los terminos de uso",
+      width: 600,
+      padding: "3em",
+      color: "#fff",
+      background: "#004671 url(/images/trees.png)",
+      backdrop: `
           url("/images/nyan-cat.gif")
           left top
           no-repeat
-        `
-      })
+        `,
+    });
 
-    }
-  
-    // return true; // Si todos los campos son válidos, enviar el formulario
+  } else {
+    var usuarioJS = {
+      usuario: user,
+      correo: email,
+      pass: password,
+      pass2: password2,
+      terminosCheck: terminos,
+    };
+
+    var stringJSON = JSON.stringify(usuarioJS);
+    Swal.fire({
+      icon: "info",
+      title: "Datos usuario",
+      text: stringJSON,
+    });
+    document.getElementById("formulario").reset();
   }
-  
+
+  // Si todos los campos son válidos, enviar el formulario
+}
